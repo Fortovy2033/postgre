caedd15c9eb9f27f177313c68ac3496c9b097fc6
Yaroslav Ermolaev <dmitryvictor2018@gmail.com>
2026-01-31 23:06:50 +0700

    add: db conn, create, insert

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "study/postgres"
+import (
+	"context"
+	"study/postgres/connection"
+	"study/postgres/sql"
+)
 
 func main() {
-	postgres.CheckConnection()
+	ctx := context.Background()
+
+	conn, err := connection.CreateConnection(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := sql.CreateTable(ctx, conn); err != nil {
+		panic(err)
+	}
+
+	if err := sql.InsertRow(ctx, conn, "homework", "p123, ex4"); err != nil {
+		panic(err)
+	}
 }
