794540fb79ab9ba4d1107803581ef8421d5c5203
Yaroslav Ermolaev <dmitryvictor2018@gmail.com>
2026-02-01 23:50:25 +0700

    refactor: postgres/sql -> sql.go

--- main.go
+++ main.go
@@ -18,19 +18,38 @@ func main() {
 	// 	panic(err)
 	// }
 
-	// if err := sql.InsertRow(ctx, conn, "homework", "p123, ex4"); err != nil {
+	// if err := sql.InsertRow(ctx, conn, sql.NewTask("завтрак", "овсяные хлопья")); err != nil {
 	// 	panic(err)
 	// }
 
-	// if err := sql.UpdateRow(ctx, conn, 2); err != nil {
+	// if err := sql.UpdateTask(ctx, conn, sql.TaskModel{}); err != nil {
 	// 	panic(err)
 	// }
 
-	// if err := sql.DeleteRow(ctx, conn, 1); err != nil {
+	// if err := sql.DeleteRow(ctx, conn, []int{2,4}); err != nil {
+	// 	panic(err)
+	// }
+
+	// if err := sql.CompleteTask(ctx, conn, 3); err != nil {
+	// 	panic(err)
+	// }
+
+	// if err := sql.UncompleteTask(ctx, conn, 3); err != nil {
 	// 	panic(err)
 	// }
 
 	if err := sql.SelectRows(ctx, conn); err != nil {
 		panic(err)
 	}
+
+	// if err := sql.CompleteTask(ctx, conn, 3); err != nil {
+	// 	panic(err)
+	// }
+
+	// task, err := sql.GetRow(ctx, conn, 3)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// sql.PrintTask(task)
 }
--- postgres/sql/models.go
+++ postgres/sql/models.go
@@ -1,6 +1,9 @@
 package sql
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type TaskModel struct {
 	ID          int
@@ -9,4 +12,26 @@ type TaskModel struct {
 	Completed   bool
 	CreatedAt   time.Time
 	CompletedAt *time.Time
+}
+
+type InsertTask struct {
+	Title       string
+	Description string
+}
+
+func NewTask(title, description string) InsertTask {
+	return InsertTask{
+		Title:       title,
+		Description: description,
+	}
+}
+
+func PrintTask(t TaskModel) {
+	fmt.Println("---------------------------")
+	fmt.Println("id:", t.ID)
+	fmt.Println("title:", t.Title)
+	fmt.Println("description:", t.Description)
+	fmt.Println("completed:", t.Completed)
+	fmt.Println("created at:", t.CreatedAt)
+	fmt.Println("completed at:", t.CompletedAt)
 }
